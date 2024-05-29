8b25e37c7fa2a4db3e36072928663f1155c606fb
hamid_tahir <hamidtahir2000@gmail.com>
2024-05-29 10:47:56 +0500

    Sticky to all templates

--- assets/custom.js
+++ assets/custom.js
@@ -1,4 +1,4 @@
-$('.swatch.option1 .swatch-element .swatchLbl').click(function(){
+/*$('.swatch.option1 .swatch-element .swatchLbl').click(function(){
   $('.swatch.option2 .swatch-element:not(.soldout):nth-of-type(1) input').click();
    var variantType=$(this).text().toLowerCase();
   console.log("work");
@@ -7,34 +7,32 @@ $('.swatch.option1 .swatch-element .swatchLbl').click(function(){
   $('.'+variantType).show();
   $('.option2 .swatch-element:not(soldout) input').click();
 });
-
-
-
+*/
 
 /***** Hamid ***/
 /*** Sticky ATC ***/
-  const getDistanceToTop = function (element) {
-    let distance = element.offsetHeight;
-    while (element) {
-      distance += element.offsetTop;
-      element = element.offsetParent;
-    }
-    return distance;
-  };
-  
-  if(document.querySelector('.grid.product-single')){
-    window.addEventListener("scroll", function () {
+const getDistanceToTop = function (element) {
+  let distance = element.offsetHeight;
+  while (element) {
+    distance += element.offsetTop;
+    element = element.offsetParent;
+  }
+  return distance;
+};
+
+if (document.querySelector(".grid.product-single")) {
+  window.addEventListener("scroll", function () {
     const stickyButton = document.querySelector(".stickyATC");
     if (stickyButton) {
       const buttonParent = stickyButton.parentElement;
       const distanceToTop = getDistanceToTop(buttonParent);
       if (window.pageYOffset > distanceToTop) {
-           stickyButton.classList.add('visible')
+        stickyButton.classList.add("visible");
       } else {
-           stickyButton.classList.remove('visible')
+        stickyButton.classList.remove("visible");
       }
     }
   });
-  }
+}
 /*** Sticky ATC End***/
 /***** Hamid End***/
